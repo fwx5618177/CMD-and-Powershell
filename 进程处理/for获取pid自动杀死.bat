fe9fb3db85e87ebd1fb8e4cc4d89dfe2d7c58e90
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-10 18:28:07 +0900

    modify taskkill func

--- 进程处理/for获取pid自动杀死.bat
+++ 进程处理/for获取pid自动杀死.bat
@@ -1,6 +1,6 @@
 @echo off
-set /p str=想要终结的程序：
-for /f "tokens=1,2* delims=\ " %%i in ('tasklist^|findstr /i cmd') do (
+set /p str=想要终结的程序名字：
+for /f "tokens=1,2* delims=\ " %%i in ('tasklist^|findstr /i %str%') do (
 	echo %%i
 	echo %%j
 	echo 即将杀死该进程

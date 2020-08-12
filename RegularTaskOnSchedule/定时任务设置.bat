558740fe52386aaac82f8dd2ee8a388aff638027
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-13 01:11:44 +0900

    add func: 定时任务设置.bat in 2020/08/13- 1:11:41.96

---
+++ RegularTaskOnSchedule/定时任务设置.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "env=D:\codedata\CMD\RegularTaskOnSchedule\env.txt"
+
+set /a count=0
+for /f %%i in (%env%) do (
+	set /a count+=1
+	if !count!==1 set pathStr=%%i
+	echo * %%i
+	echo !pathStr!
+)
+
+echo.
+::schtasks /create /sc DAILY /tn "TASK BAT Upload files" /TR !path!\RegularTaskOnSchedule\全自动上传.bat /ST 22:00
+schtasks /create /sc minute /tn "TASK BAT Upload files" /TR "!path!\RegularTaskOnSchedule\全自动上传.bat"
+
+
+pause > nul

d07485fdc0e685ed2fbe4407c1c4c415df7ef8b5
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-23 00:59:29 +0900

    add func: 触发器.bat in 2020/08/23- 0:59:28.06

--- 热词查询网/触发器.bat
+++ 热词查询网/触发器.bat
@@ -1,1 +1,13 @@
 @echo off && setlocal enabledelayedexpansion
+
+:start
+set /p "FILEPATH=输入你的文件地址："
+if "%FILEPATH%"=="" goto start
+set /p "RESULTFILE=结果存放地址："
+if "%RESULTFILE%"=="" goto start
+
+powershell -file GetSearch.ps1 %FILEPATH% %RESULTFILE%
+
+echo.
+echo 结束
+pause>nul

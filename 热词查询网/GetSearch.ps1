84080d9cceb9a9cb73e69a1616ba62ad0ef0d496
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-23 00:59:29 +0900

    add func: GetSearch.ps1 in 2020/08/23- 0:59:28.06

---
+++ 热词查询网/GetSearch.ps1
@@ -0,0 +1,86 @@
+function GetData() {
+    param(
+    [Parameter(Mandatory)]
+    [string]
+    $Path,
+    [Parameter(Mandatory)]
+    [string]
+    $Result
+    )
+
+
+    process{
+      Get-Content $Path | ForEach-Object {
+            $_
+            $keywords = @()
+            $RandomNum = @()
+            #url: https://data.chinaz.com/
+            #search: /keyword/analysis/
+            # https://data.chinaz.com/keyword/analysis/%E6%96%97%E9%B1%BC
+
+            $url = "https://data.chinaz.com/keyword/analysis/$_"
+
+            $web = Invoke-WebRequest -Uri $url -Method Get
+            #Invoke-WebRequest -Uri $url -Method Get -OutFile tmp.txt
+            #$web = Get-Content tmp.txt           
+            
+            #获取内容
+            ([regex]"col-225.+?li>").Matches($web.content) | ForEach-Object {
+                if([string]::IsNullOrEmpty($_)){
+                    return
+                }else {
+                    $keywords += ((([regex]"<a.+?>(.+?)</a>").Matches($_)) -replace "<.+?>","") -split "\s"
+                }               
+            }
+            $RandomNum = RandomNum -Lth $keywords.Length -Num 3
+
+            $String = $keywords[$RandomNum[0]] +"_"+ $keywords[$RandomNum[1]] +"_"+ $keywords[$RandomNum[2]]
+
+            Write-Host $String -BackgroundColor Red -ForegroundColor Cyan
+            Write-Output $String | Out-File -Append $Result
+        }
+        
+
+
+    }
+}
+
+function RandomNum(){
+    param(
+        [Parameter(Mandatory)]
+        [int]
+        $Num,
+        [Parameter(Mandatory)]
+        [int]
+        $Lth
+    )
+    begin {
+        $arr= @()
+    }
+    process {
+
+        for($i=0; $i -le 10; $i++){
+            $RandomNum = Get-Random -Minimum 1 -Maximum $Lth
+            if($arr.Contains($RandomNum)){
+                continue
+            }else {
+                $arr+=$RandomNum
+            }
+
+            if($arr.length -eq $Num){
+                break;
+            }
+
+        }
+
+        return $arr
+    }
+
+}
+
+$Path = $args[0]
+$Result = $args[1]
+#$Path = "D:\codedata\CMD\热词查询网\search.txt"
+#$Result = "D:\codedata\CMD\热词查询网\Result.txt"
+
+GetData -Path $Path -Result $Result

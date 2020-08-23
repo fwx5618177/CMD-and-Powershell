b1fb52a3730bf0c37fa1181c84c4c2db7afaec7a
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-23 22:30:09 +0900

    add func: 高效率版-字典生成器.bat in 2020/08/23-22:30:01.64

---
+++ 生成字典/高效率版-字典生成器.bat
@@ -0,0 +1,14 @@
+@echo off && setlocal enabledelayedexpansion
+
+set "str=a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+
+SET NUM=100
+SET LTH=9
+
+(FOR /L %%j in (1,1,%NUM%) do (
+		set p=
+		for /l %%i in (1,1,%l%) do (
+			
+		)
+) 
+)
